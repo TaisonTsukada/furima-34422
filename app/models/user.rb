6f20010ca6fdfae26e99001d74df4fa22406a555
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-02-06 16:11:57 +0900

    バリデーションの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  with_options presence: true, format: { with:  /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: ' Full-width characters'} do
+    validates :family_name, presence: true
+    validates :first_name, presence: true
+  end
+  with_options presence: true, format: { with: /\A[ア-ン゛゜ァ-ォャ-ョー「」、]+\z/, message: 'Full-width katakana characters'} do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
   validates :birth_day, presence: true
-  
 end

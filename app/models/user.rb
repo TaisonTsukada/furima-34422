249b7d2d5d6a081c22d0114d227eba89fa24fb8f
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-02-07 10:51:26 +0900

    訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: ' Full-width characters' } do
-    validates :family_name, presence: true
-    validates :first_name, presence: true
+    validates :family_name
+    validates :first_name
   end
   with_options presence: true, format: { with: /\A[ア-ン゛゜ァ-ォャ-ョー「」、]+\z/, message: 'Full-width katakana characters' } do
     validates :family_name_kana
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,25 +9,6 @@ RSpec.describe User, type: :model do
       it '全ての項目が正しく入力されていたら登録できる' do
         expect(@user).to be_valid
       end
-      it 'passwordとpassword_confirmationが6文字以上だったら登録できる' do
-        @user.password = 'A12345'
-        @user.password_confirmation = 'A12345'
-        expect(@user).to be_valid
-      end
-      it 'ユーザーの本名が全角で入力されていたら登録できる' do
-        @user.first_name = '田中'
-        @user.family_name = '大輔'
-        expect(@user).to be_valid
-      end
-      it 'ユーザー本名のフリガナがカタカナであったら登録できる' do
-        @user.family_name_kana = 'タナカ'
-        @user.first_name_kana = 'ダイスケ'
-        expect(@user).to be_valid
-      end
-      it '生年月日が入力されていたら登録できる' do
-        @user.birth_day = '1980-07-12'
-        expect(@user).to be_valid
-      end
     end
     context '新規登録できないとき' do
       it 'nicknameがないと登録できない' do
@@ -96,18 +77,25 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password Include both letters and numbers')
       end
-      it 'family_nameとfirst_nameが全角でないと登録できない' do
+      it 'family_nameが全角でないと登録できない' do
         @user.family_name = 'yamada'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name  Full-width characters")
+      end
+      it 'first_nameが全角でないと登録できない' do
         @user.first_name = 'tarou'
         @user.valid?
-        expect(@user.errors.full_messages).to include('Family name  Full-width characters', 'First name  Full-width characters')
+        expect(@user.errors.full_messages).to include("First name  Full-width characters")
       end
-      it 'family_name_kanaとfirst_name_kanaがカタカナ入力でないと登録できない' do
+      it 'family_name_kanaがカタカナ入力でないと登録できない' do
         @user.family_name_kana = 'やまだ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Family name kana Full-width katakana characters')
+      end
+      it 'first_name_kanaがカタカナ入力でないと登録できない' do
         @user.first_name_kana = 'たろう'
         @user.valid?
-        expect(@user.errors.full_messages).to include('Family name kana Full-width katakana characters',
-                                                      'First name kana Full-width katakana characters')
+        expect(@user.errors.full_messages).to include('First name kana Full-width katakana characters')
       end
     end
   end

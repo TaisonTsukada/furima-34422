6b81306d5300297e5c0380842e25c6a4df23b7b0
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-02-07 22:12:37 +0900

    新規投稿機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,14 +4,14 @@ class Item < ApplicationRecord
   belongs_to :condition
   belongs_to :delivery_fee
   belongs_to :area
-  belongs_to :delivery_days
+  belongs_to :delivery_day
   has_one_attached :image
 
   validates :title, presence: true
   validates :description, presence: true
   validates :category_id, numericality: { other_than: 1 } 
   validates :condition_id, numericality: { other_than: 1 } 
-  valodates :delivery_fee_id, numericality: { other_than: 1 } 
+  validates :delivery_fee_id, numericality: { other_than: 1 } 
   validates :area_id, numericality: { other_than: 1 } 
   validates :delivery_days_id, numericality: { other_than: 1 } 
   validates :price, presence: true

e566df64c9ea5cebf1861b74dc5c9566275a3487
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-02-08 19:44:34 +0900

    商品一覧表示機能の実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,20 +1,18 @@
 FactoryBot.define do
   factory :item do
-    title {Faker::Lorem.sentence}
-    description {Faker::Lorem.sentence}
-    price {3000}
-    category_id {2}
-    condition_id {3}
-    delivery_fee_id {2}
-    area_id {6}
-    delivery_days_id {3}
-    
+    title { Faker::Lorem.sentence }
+    description { Faker::Lorem.sentence }
+    price { 3000 }
+    category_id { 2 }
+    condition_id { 3 }
+    delivery_fee_id { 2 }
+    area_id { 6 }
+    delivery_days_id { 3 }
+
     association :user
-    
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
-
   end
 end

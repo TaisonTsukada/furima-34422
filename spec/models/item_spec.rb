ffa320781f21e3386f4d5f28f4c83dbc4df0f92f
TaisonTsukada <tukadataison1208@i.softbank.jp>
2021-02-08 17:42:05 +0900

    修正2

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -62,7 +62,7 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
-      it '販売価格が9999999円以上だったら出品できない' do
+      it '販売価格が10000000円以上だったら出品できない' do
         @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
